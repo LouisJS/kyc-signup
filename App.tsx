8cf787d8abd65c6a7b0e178cb4e9a3868bb314a8
Louis Lecocq <lecocq.pro@gmail.com>
2024-01-27 20:45:00 +0100

    feat(font) add fonts assets and load in App

--- App.tsx
+++ App.tsx
@@ -1,7 +1,36 @@
 import 'react-native-gesture-handler';
 
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
 import RootStack from './src/navigation';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
-  return <RootStack />;
+  const [fontsLoaded, fontError] = useFonts({
+    'Inter-Medium': require('./assets/fonts/Inter-Medium.ttf'),
+    'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
+    'Raleway-ExtraBold': require('./assets/fonts/Raleway-ExtraBold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView className="flex flex-1" onLayout={onLayoutRootView}>
+        <RootStack />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
