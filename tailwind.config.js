f74121344bc043c99ff4f2dc083001e15fcccae8
Louis Lecocq <lecocq.pro@gmail.com>
2024-01-27 20:44:26 +0100

    feat(tailwind) setup tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,30 @@ module.exports = {
   content: ['./App.{js,ts,tsx}', './src/**/*.{js,ts,tsx}'],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          default: '#4D50F4',
+          lighter: '#CACBFC',
+        },
+        merino: '#F4EADF',
+        grey: {
+          150: '#EDEEFE',
+          800: '#212836',
+        },
+      },
+      fontFamily: {
+        'raleway-extrabold': ['Raleway-ExtraBold'],
+        'inter-medium': ['Inter-Medium'],
+        'inter-bold': ['Inter-Bold'],
+      },
+      fontSize: {
+        '3xl': ['28px', '32.87px'],
+      },
+      borderRadius: {
+        lg: '10px',
+      },
+    },
   },
   plugins: [],
 };
